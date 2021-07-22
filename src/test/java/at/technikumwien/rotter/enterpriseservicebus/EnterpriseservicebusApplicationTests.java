7d09735c64e8c9a91824c586d1bffa1407638da8
if18b136 <if18b136@technikum-wien.at>
2021-07-22 21:55:58 +0200

    Initial commit

---
+++ src/test/java/at/technikumwien/rotter/enterpriseservicebus/EnterpriseservicebusApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikumwien.rotter.enterpriseservicebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseservicebusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
